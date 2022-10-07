77c4ee3f77be2e4d1d2100bc017d9b8840a20044
Idara umana15 <idaraedet15@yahoo.com>
2022-10-07 15:15:30 -0700

    storing file with originalname and ext

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, FileTypeValidator, HttpStatus, MaxFileSizeValidator, ParseFilePipe, Post, Res, UploadedFile, UseInterceptors} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
+import * as path from 'path';
+import { UserService } from './user.service';
+
 
 
 @Controller('user')
 export class UserController {
-UserService: any;
-
+  // constructor(private readonly userService:UserService){}
 
     //without validation
 //     @Post('upload')
@@ -19,21 +21,56 @@ UserService: any;
 
 
 //how to validate your files
+// @Post('upload')
+// @UseInterceptors(FileInterceptor('file'))
+// uploadFile(@UploadedFile(
+// new ParseFilePipe({
+//     validators: [
+//       new MaxFileSizeValidator({ maxSize: 10000000 }),
+//       new FileTypeValidator({ fileType: 'jpeg' }),
+//     ],
+//   }),
+// )
+// file: Express.Multer.File) {
+//   // console.log(file)
+
+// }
+
 @Post('upload')
-@UseInterceptors(FileInterceptor('file',{
-  storage:diskStorage({})
+@UseInterceptors(FileInterceptor('image',{
+  storage: diskStorage({
+    destination: './src/uploadedFiles',
+    filename: (req, file, callBack) => {
+      //storing the file originalname
+      const fileName = path.parse(file.originalname).name.
+      replace(/\s/g, '') +Date.now();
+
+      //storing the file extension
+const extension = path.parse(file.originalname).ext;
+callBack(null, `${fileName}${extension}`);
+    }
+  })
 }))
-uploadFile(@UploadedFile(
-new ParseFilePipe({
-    validators: [
-      new MaxFileSizeValidator({ maxSize: 10000000 }),
-      new FileTypeValidator({ fileType: 'jpeg' }),
-    ],
+
+//sending a response
+fileUpload(@Res()res, @UploadedFile(
+
+  //adding validation to the file
+  new ParseFilePipe({
+      validators: [
+          new MaxFileSizeValidator({ maxSize:1000000}),
+          new FileTypeValidator({ fileType: 'jpeg'}),
+      ],
   }),
-)
-file: Express.Multer.File) {
-  // console.log(file)
-  return this.UserService.saveFile(file);
+)file){
+  //returning an HTTP response to the user with the file path
+  return res.status(HttpStatus.OK).json({
+      success:true,
+      data: file.path
+  })
 }
+
+
+
 
 }
